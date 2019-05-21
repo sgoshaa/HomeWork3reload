7e5dff59c3bd0ae015721afce113207578242b4c
sgoshaa <sgoshaa@yandex.ru>
2019-05-21 23:19:31 +0500

    Добавлена БД

--- HomeWork3reload/Infrastructure/UserInfoService.cs
+++ HomeWork3reload/Infrastructure/UserInfoService.cs
@@ -4,24 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using HomeWork3reload.Models;
 using HomeWork3reload.Services;
+using Npgsql;
+using Dapper;
+
 namespace HomeWork3reload.Infrastructure
 {
     public class UserInfoService:IUserInfoService
     {
-        public async Task<User> GetById(Guid id)
-        {
-            User user = new User
-            {
-                Email = "test@test.ru",
-                Id = id,
-                Nickname = "test",
-                Phone = "+7 987 654 32 10"
-            };
-
-            return await Task.FromResult<User>(user);
-        }
-        /* private const string ConnectionString =
-            "host=localhost;port=5432;database=postgres;username=postgres;password=1";
+       
+         private const string ConnectionString =
+            "host=localhost;port=5432;database=homework3;username=postgres;password=1";
 
          public async Task<User> GetById(Guid id)
          {
@@ -30,7 +22,7 @@ namespace HomeWork3reload.Infrastructure
                  return await connection.QuerySingleAsync<User>(
                      "SELECT * FROM Users WHERE Id = @id", new { id });
              }
-         }*/
+         }
 
     }
 }
